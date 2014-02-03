50399fbd18ef47c1adf9a8c927da4ce92de88bbb
Junji Hashimoto <junji.hashimoto@gree.net>
2014-02-04 02:44:09 +0900

    Added packagedb-support

--- ghc-mod.sh
+++ ghc-mod.sh
@@ -1,11 +1,45 @@
 #!/usr/bin/env bash
+# hack for Yesod
+ 
+ghc_mod=~/.cabal/bin/ghc-mod
+ 
+pwd=`pwd`
+dir=`basename $pwd`
+#set -x
+EXT="-g-XHaskell98 -g-XTemplateHaskell -g-XQuasiQuotes -g-XOverloadedStrings -g-XNoImplicitPrelude -g-XCPP -g-XMultiParamTypeClasses -g-XTypeFamilies -g-XGADTs -g-XGeneralizedNewtypeDeriving -g-XFlexibleContexts -g-XEmptyDataDecls -g-XNoMonomorphismRestriction -g-XDeriveDataTypeable"
 
-export PATH=/home/junji/.cabal/bin:$PATH
+function find_config(){
+		local DIR=`pwd`
+		while [ ! $DIR = "/" ] ;do
+				if [ -e "$DIR/cabal.sandbox.config" ];then
+						echo "$DIR/cabal.sandbox.config"
+						return 0
+				fi
+				DIR=`dirname $DIR`
+		done
+		echo ""
+		return 1
+}
 
-if [ "$1" = check ] ; then
-    shift
-    hdevtools check $@ | lineconv.pl
+function ghcmod(){
+		if [ "$1" = check ] ; then
+				shift
+				PACKAGEDB=""
+				SCONFIG=`find_config`
+				if [ ! -z "$SCONFIG" ]; then
+						PACKAGEDB=" $EXT -g-no-user-package-db -g-package-db="`perl -ane 'if(/package-db:(\s*)(.*)$/){print \$2}' < $SCONFIG`" "
+				fi
+				hdevtools check $PACKAGEDB $@ | lineconv.pl
+		else
+				ghc-mod  $@
+		fi
+}
+
+ 
+if [ -e "../Handler" -a -d ../app ]  ; then
+    pushd .. > /dev/null
+    ghcmod $@
+    popd > /dev/null
 else
-    ghc-mod  $@
+    ghcmod $@
 fi
-#ghc-mod  $@
